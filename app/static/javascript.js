32c8d16116fa84afded1d9dce5efcc44be884114
gabicantarini <gabilana64@hotmail.com>
2022-02-08 22:00:32 +0000

    feat: add new readme info

--- app/static/javascript.js
+++ app/static/javascript.js
@@ -1,11 +1,43 @@
 (function(win,doc){
     'use strict';
 
+    //confirm ir the user wants to delete data
     if(doc.querySelector('.btnDel')){
         let btnDel = doc.querySelectorAll('.btnDel');
-        for(let i=0; i < btnDel.length, i++){
-            btnDel[i].add
+        for(let i=0; i < btnDel.length; i++){
+            btnDel[i].addEventListener('click', function(event){
+                if(confirm('Do you want to delete this data?')){
+                    return true;
+                }else{
+                    event.preventDefault();
+                };
+            });
+
+        };
+    };
+
+    //ajax do form
+    if(doc.querySelector('#form')){
+        let form=doc.querySelector('#form');
+        function sendForm(event)
+        {
+            preventDefault();
+            let data = new FormData(form);
+            let ajax = new HMLHttpRequest();
+            let token = doc.querySelectorAll('input')[0].value;
+
+            ajax.open('POST', form.action);
+            ajax.setRequestHeader('X-CSRF-TOKEN',token);
+            ajax.onreadystatechange = function()
+            {
+                if(ajax.status === 200 && ajax.readyStaty === 4){
+                    console.log('Cadastrou!');
+                }
+            }
+            ajax.send(data);
 
         }
+        form.addEventListener('submit', sendForm, false);
     }
+
 })(window,document);
--- readme.md
+++ readme.md
@@ -10,3 +10,11 @@
 ### Deleting unused or unnecessary vehicles
 
 ## ROUTE
+####    path('admin/', admin.site.urls),
+####    path('', home, name='home'),
+####    path('form/', form, name='form'),
+####    path('create/', create, name='create'),
+####    path('view/<int:pk>/', view, name='view'),
+####    path('edit/<int:pk>/', edit, name='edit'),
+####    path('update/<int:pk>/', update, name='update'),
+####    path('delete/<int:pk>/', delete, name='delete'),
